c9b6917d3f140138d76cf2ac587a1d1cafce328a
unknown <halljosua@web.de>
2023-02-11 18:56:27 +0100

    osternacht23

--- src/components/ProjectDetail3.jsx
+++ src/components/ProjectDetail3.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import mlsBild1 from "../pictures/home/mls_plakat.jpg";
 import mlsBild2 from "../pictures/home/mls_band.JPG";
+import pic_osternacht23 from "../pictures/home/osternacht23.png";
 
 function ProjectDetail3() {
   return (
@@ -9,7 +10,22 @@ function ProjectDetail3() {
         <div className="content py-2">
           <div className="bands-card-layout mt-2 p-1">
             <h1>Osternacht Mitteltal 2023</h1>
-            <p>Infos demnächst hier</p>
+            <p>Ostersonntag - Christuskirche Mitteltal</p>
+            <div className="">
+              <img
+                src={pic_osternacht23}
+                className="project-card-image"
+                alt=""
+              />
+            </div>
+            <p>
+              Im Dunkel unsrer Nacht entzünde das Feuer, dass nie mehr erlischt
+            </p>
+            <p>
+              Mit einigen Instrumentalisten und einem Projektchor wollen wir die
+              Osternacht in der Christuskirche in Mitteltal feiern.
+              Voraussichtlicher Beginn ist 6.00Uhr, vor dem Sonnenaufgang.
+            </p>
           </div>
         </div>
       </div>
